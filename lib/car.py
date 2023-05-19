42b67542036249b1312612141a83ad9752c0465a
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-19 13:04:16 +0300

    Completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    def fill_up_tank(self):
+        return "filling up!"
     pass
